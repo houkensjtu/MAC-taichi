2453ab12b1cda77f220aedbabe853786e085db91
Qian Bao <houkensjtu@gmail.com>
2022-08-17 10:43:29 +0800

    Update vs Numba benchmark.

--- mac-benchmark.py
+++ mac-benchmark.py
@@ -5,9 +5,10 @@ import time
 
 ti.init(arch=ti.gpu, kernel_profiler=True)
 
-lx, ly, nx, ny = 1.0, 1.0, 512, 512
+lx, ly, nx, ny = 1.0, 1.0, 2048, 2048
 dx, dy, dt = lx/nx, ly/ny, 0.001
 mu = 0.001
+repeat = 5000
 
 u = ti.field(dtype=float, shape=(nx+1,ny+2))
 ut = ti.field(dtype=float, shape=(nx+1,ny+2))
@@ -33,46 +34,48 @@ def calc_velocity():
            +(mu) * ((u[i+1,j] - 2 * u[i,j] + u[i-1,j]) / dx ** 2
            +(u[i,j+1] - 2 * u[i,j] + u[i,j-1]) / dy ** 2))
     for i,j in ti.ndrange((1,nx+2),(1,ny+1)):
-        vt[i,j] = v[i,j] + dt * ((-0.25)*\
-          (((u[i,j+1] + u[i,j]) * (v[i+1,j]+v[i,j])\
-          -(u[i-1,j+1] + u[i-1,j]) * (v[i,j]+v[i-1,j])) / dx\
-          +((v[i,j+1]+v[i,j]) ** 2-(v[i,j]+v[i,j-1]) ** 2) / dy)\
-          +(mu)*((v[i+1,j] - 2 * v[i,j] + v[i-1,j]) / dx ** 2\
+        vt[i,j] = v[i,j] + dt * ((-0.25)*
+          (((u[i,j+1] + u[i,j]) * (v[i+1,j]+v[i,j])
+          -(u[i-1,j+1] + u[i-1,j]) * (v[i,j]+v[i-1,j])) / dx
+          +((v[i,j+1]+v[i,j]) ** 2-(v[i,j]+v[i,j-1]) ** 2) / dy)
+          +(mu)*((v[i+1,j] - 2 * v[i,j] + v[i-1,j]) / dx ** 2
           +(v[i,j+1] - 2 * v[i,j] + v[i,j-1]) / dy ** 2))
 
 init()        
-for i in range(1000):
+for i in range(repeat):
     calc_velocity()
     
 ti.profiler.print_kernel_profiler_info()
 
-u_nb = np.ones((nx+1, ny+2)).astype(np.float32)
-v_nb = np.ones((nx+2, ny+1)).astype(np.float32)
-ut_nb = np.ones((nx+1, ny+2)).astype(np.float32)
-vt_nb = np.ones((nx+2, ny+1)).astype(np.float32)
+# Numba version below
+init() # Rewrite the fields
+u_nb = u.to_numpy() # Convert to ndarray
+v_nb = v.to_numpy()
+ut_nb = ut.to_numpy()
+vt_nb = vt.to_numpy()
 
-@njit
-def calc_velocity_numba(u_nb, v_nb, ut_nb, vt_nb):
+@njit(parallel=True, fastmath=True)
+def calc_velocity_numba(u, v, ut, vt):
     for i in prange(1,nx+1):
         for j in prange(1,ny+2):
-            ut_nb[i,j] = u_nb[i,j] + dt * ((-0.25)*
-           (((u_nb[i+1,j] + u_nb[i,j])**2 - (u_nb[i,j] + u_nb[i-1,j])**2) / dx
-           +((u_nb[i,j+1] + u_nb[i,j]) * (v_nb[i+1,j] + v_nb[i,j])
-           -(u_nb[i,j] + u_nb[i,j-1]) * (v_nb[i+1,j-1] + v_nb[i,j-1])) / dy)
-           +(mu) * ((u_nb[i+1,j] - 2 * u_nb[i,j] + u_nb[i-1,j]) / dx ** 2
-           +(u_nb[i,j+1] - 2 * u_nb[i,j] + u_nb[i,j-1]) / dy ** 2))
+            ut[i,j] = u[i,j] + dt * ((-0.25)*
+               (((u[i+1,j] + u[i,j])**2 - (u[i,j] + u[i-1,j])**2) / dx
+               +((u[i,j+1] + u[i,j]) * (v[i+1,j] + v[i,j])
+               -(u[i,j] + u[i,j-1]) * (v[i+1,j-1] + v[i,j-1])) / dy)
+               +(mu) * ((u[i+1,j] - 2 * u[i,j] + u[i-1,j]) / dx ** 2
+               +(u[i,j+1] - 2 * u[i,j] + u[i,j-1]) / dy ** 2))
     for i in prange(1,nx+2):
         for j in prange(1,ny+1):
-            vt_nb[i,j] = v_nb[i,j] + dt * ((-0.25)*\
-          (((u_nb[i,j+1] + u_nb[i,j]) * (v_nb[i+1,j]+v_nb[i,j])\
-          -(u_nb[i-1,j+1] + u_nb[i-1,j]) * (v_nb[i,j]+v_nb[i-1,j])) / dx\
-          +((v_nb[i,j+1]+v_nb[i,j]) ** 2-(v_nb[i,j]+v_nb[i,j-1]) ** 2) / dy)\
-          +(mu)*((v_nb[i+1,j] - 2 * v_nb[i,j] + v_nb[i-1,j]) / dx ** 2\
-          +(v_nb[i,j+1] - 2 * v_nb[i,j] + v_nb[i,j-1]) / dy ** 2))
+            vt[i,j] = v[i,j] + dt * ((-0.25)*
+               (((u[i,j+1] + u[i,j]) * (v[i+1,j]+v[i,j])
+               -(u[i-1,j+1] + u[i-1,j]) * (v[i,j]+v[i-1,j])) / dx
+               +((v[i,j+1]+v[i,j]) ** 2-(v[i,j]+v[i,j-1]) ** 2) / dy)
+               +(mu)*((v[i+1,j] - 2 * v[i,j] + v[i-1,j]) / dx ** 2
+               +(v[i,j+1] - 2 * v[i,j] + v[i,j-1]) / dy ** 2))
 
-calc_velocity_numba(u_nb, v_nb, ut_nb, vt_nb)            
-now = time.time()
-for i in range(1000):
+calc_velocity_numba(u_nb, v_nb, ut_nb, vt_nb) # Skip the first run
+now = time.perf_counter()
+for i in range(repeat):
     calc_velocity_numba(u_nb, v_nb, ut_nb, vt_nb)
-end = time.time()
-print('Time spent', end -now)
+end = time.perf_counter()
+print(f'Time spent in Numba(CPU) for {repeat}x: {(end -now):4.2f} sec.')
